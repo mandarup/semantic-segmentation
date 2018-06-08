16871d826505874e20874cb0a445f0592f85078e
mandarup <mandarupadhye@gmail.com>
2018-06-08 12:08:43 -0400

    fix float

--- main.py
+++ main.py
@@ -99,6 +99,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
 tests.test_layers(layers)
 
 
+
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     Build the TensorFLow loss and optimizer operations.
@@ -150,7 +151,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
             _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image: image,
                                                                           correct_label: label,
                                                                           keep_prob: 0.5,
-                                                                          learning_rate: 1e-4})
+                                                                          learning_rate: .0004})
             avg += loss
             counter += 1
 
